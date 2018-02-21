94d0576462d5af8df5fefc6feb9cc0d88e90ae00
Isabelle <iglee@uw.edu>
2018-02-21 02:19:05 -0800

    bubble sort

--- src/sort_search.py
+++ src/sort_search.py
@@ -11,3 +11,14 @@ def binary_search(A, x, low, high):
             return binary_search(A, x, low, high)
     else:
         print "element not found"
+
+def bubble_sort(items):
+    switch = False
+    for i in range(len(items)-1):
+        if items[i] > items[i+1]:
+            items[i], items[i+1] = items[i+1], items[i]
+            switch = True
+    if switch == False:
+        return items
+    else:
+        return bubble_sort(items)
